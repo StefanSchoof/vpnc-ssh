05e12441028d9aeb91dfb81b17023a434f2f9f17
Ubuntu <sshazure@mylinuxvn.3ngqw0qhr42uhmx5lywijukzab.ax.internal.cloudapp.net>
2018-10-23 17:27:41 +0000

    intail version

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM debian:stable
+
+RUN apt-get update && apt-get install -y \
+    vpnc \
+    openssh-client \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY entrypoint.sh /
+RUN mkdir /root/.ssh \
+ && chmod 700 /root/.ssh
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["ssh"]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+variables='GATEWAY USERNAME SECRET PASSWORD SSHKEY KNOWNHOSTS'
+missingVar=false
+for var in $variables
+do
+  if [ -z "${!var}" ]
+  then
+    missingVar=true
+    echo "$var is missing or has no value"
+  fi
+done
+if [ "$missingVar" = true ]
+then
+  exit 1
+fi
+cat > /etc/vpnc/vpn.conf << EOF
+IPSec gateway $GATEWAY
+IPSec ID $USERNAME
+IPSec secret $SECRET
+IKE Authmode psk
+Xauth username $USERNAME
+Xauth password $PASSWORD
+local port 0
+DPD idle timeout (our side) 0
+EOF
+echo "$SSHKEY" > /root/.ssh/id_rsa
+echo "$KNOWNHOSTS" > /root/.ssh/known_hosts
+vpnc vpn
+exec "$@"
